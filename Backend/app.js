8e83091023afff52b2ccdcef93e9e1ce761c3fc4
omkundu <omkundu156@gmail.com>
2022-12-15 22:29:17 +0530

    day-3-work-done

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const passport=require("passport")
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const googleClientapi="822894406741-fvi08vfbufgj9nol3gqefrue0bktjt7m.apps.googleusercontent.com";
+const googleClientSecret="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"
 const app = express();
 const cors = require('cors');
 const UserRouter = require('./routes/UserRoutes')
 const ProductRouter = require('./routes/ProductRoutes')
+const GoogleRouter = require('./Routes/GoogleRoutes')
 
 app.use(express.json()); // JSON Middleware 
 app.use(cors({
@@ -11,7 +16,29 @@ app.use(cors({
 
 app.use('/api/v1/users',UserRouter); //ParentRoute
 app.use('/api/v1/Products',ProductRouter); 
+app.use('/api',GoogleRouter); 
 
+passport.use(new GoogleStrategy({
+    clientID:     googleClientapi,
+    clientSecret: googleClientSecret,
+    callbackURL: "http://localhost:5000/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+  
+      return done(null, profile);
+   
+  }
+));
+
+passport.serializeUser(function(user,done){
+    done(null,user)
+});
+
+
+passport.deserializeUser(function(user,done){
+  done(null,user)
+});
 app.all("*",(req,res,next)=>{
     res.status(400).json({
         status : "Fail",
